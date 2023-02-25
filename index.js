2580b62a1cb7fa6d822b02db1773c584cd31e53a
Fabricio <fabricioj.dev@gmail.com>
2023-02-24 23:07:05 -0400

    Adicionando Dark Mode e light mode

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+
+
+
 function calcular(){
   var valorAltura = document.getElementById("altura")
   var valorMassa = document.getElementById("massa")
   var mostrarResultado = document.getElementById("resultado")
+
 
   var valorSomaAltura = parseFloat(valorAltura.value)
   var valorSomaMassa =  parseFloat(valorMassa.value)
@@ -18,26 +22,19 @@ function calcular(){
     mostrarResultado.style.color = "red"
     valorAltura.style.border = "1px solid red"
   }
-
   if(valorMassa.value == ""){
     mostrarResultado.innerHTML += "Por favor preecha o campo 'Massa'"
     mostrarResultado.style.display = "block"
     mostrarResultado.style.color = "red"
     valorMassa.style.border = "1px solid red"
-  }
-  
+  }  
   if(valorAltura.value && valorMassa.value){
     mostrarResultado.innerHTML = `Seu IMC é: ${resultadoIMC.toFixed(2)} <br>`
     mostrarResultado.style.display = "block"
     valorMassa.style.border = "1px solid green"
     valorAltura.style.border = "1px solid green"
     mostrarResultado.style.color = "black"
-
-
   }
-
-
-  console.log(resultadoIMC.toFixed(2))
 
   if(resultadoIMC < 18.5){
     mostrarResultado.innerHTML += "Você é está com o IMC: ABAIXO DO PESO"
@@ -51,13 +48,22 @@ function calcular(){
     mostrarResultado.innerHTML += "Você é está com o IMC: OBESIDADE GRAVE"
   }
 
+}
 
+function mudarEscuro(){
+  let body = document.getElementById("cor")
+  let borda = document.getElementById("cores")
 
+  body.style.backgroundColor = "rgb(24, 23, 23)"
+  body.style.color = "white"
+  borda.style.border ="1px solid white"
+}
+function mudarClaro(){
+  let body = document.getElementById("cor")
+  let borda = document.getElementById("cores")
 
-
-
-
-  
-
+  body.style.backgroundColor = "white"
+  body.style.color = "black"
+  borda.style.border ="1px solid black"
 }
 
